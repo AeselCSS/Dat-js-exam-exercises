4fadec06a9e88d5963517f510320835c689c36c8
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-08-12 23:23:19 +0200

    solution

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,44 @@
 "use strict";
 
+let animals = [];
+
+window.addEventListener("load", () => {
+    console.log("Hello World");
+    document.querySelector("#create-form").addEventListener("submit", createAnimal);
+});
+
+function newAnimal(name, type, age) {
+    const newAnimal = { name, type, age };
+    animals.push(newAnimal);
+    console.log(animals);
+    showAnimals();
+}
+
+function createAnimal(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const name = form.name.value;
+    const type = form.type.value;
+    const age = form.age.value;
+
+    newAnimal(name, type, age);
+}
+
+function showAnimals() {
+    const list = document.querySelector("#list-container tbody");
+    list.innerHTML = "";
+
+    animals.sort((a, b) => a.name.localeCompare(b.name));
+
+    for (const animal of animals) {
+        const html = /*html*/ `
+        <tr>
+            <td>${animal.name}</td>
+            <td>${animal.type}</td>
+            <td>${animal.age}</td>
+        </tr>
+        `;
+        list.insertAdjacentHTML("beforeend", html);
+    }
+}
