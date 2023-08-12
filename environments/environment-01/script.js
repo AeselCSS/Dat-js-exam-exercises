9300563680369431827bf1c586e5e800e319b1f0
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-08-12 14:39:15 +0200

    solution

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,34 @@
 "use strict";
 
+window.addEventListener("load", async () => {
+    const users = await getUsers();
+    showUsers(users);
+    countRoles(users);
+});
+
+async function getUsers() {
+    const res = await fetch("users.json");
+    return res.json();
+}
+
+function showUsers(users) {
+    const list = document.querySelector("#userlist");
+    list.innerHTML = "";
+
+    for (const user of users) {
+        const html = /*html*/ `
+        <li>Name: ${user.name} - Role: ${user.role}</li>
+        `;
+        list.insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function countRoles(users) {
+    const adminCount = users.filter((user) => user.role === "admin");
+    const userCount = users.filter((user) => user.role === "user");
+    const guestCount = users.filter((user) => user.role === "guest");
+
+    document.querySelector("#admin-count").textContent = adminCount.length;
+    document.querySelector("#user-count").textContent = userCount.length;
+    document.querySelector("#guest-count").textContent = guestCount.length;
+}
