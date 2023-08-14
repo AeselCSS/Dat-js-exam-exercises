3abcc288bd7a1ff49cd7d20f96e0cdd79a6723b1
Chris Skov Sørensen <chso21ad@student.cbs.dk>
2023-08-14 07:52:16 +0200

    solution

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,43 @@
 "use strict";
 
+let animals = [
+    {name: "Mette", type: "Cat", age: 31},
+    {name: "Chris", type: "Tiger", age: 36},
+    {name: "Filippa", type: "Unicorn", age: 7},
+    {name: "August", type: "Dragon", age: 3}
+];
+
+window.addEventListener("load", () => {
+    showAnimals();
+    document.querySelector("#create-form").addEventListener("submit", createAnimal)
+})
+
+function showAnimals () {
+const list = document.querySelector("#list-container tbody");
+list.innerHTML = "";
+
+for (const animal of animals) {
+    const html = /*html*/ `
+    <tr>
+        <td>${animal.name}</td>
+        <td>${animal.type}</td>
+        <td>${animal.age}</td>
+    </tr>
+    `;
+    list.insertAdjacentHTML("beforeend", html)
+    }
+}
+
+function createAnimal(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    const newAnimal = {
+        name: form.name.value,
+        type: form.type.value,
+        age: form.age.value
+    }
+
+    animals.push(newAnimal)
+    showAnimals()
+}
